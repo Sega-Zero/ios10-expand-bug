b9d56df214731e4514ccfbbdf7ddfcea6144025b
Сергей Галездинов <segazero@gmail.com>
2016-11-23 19:48:56 +0300

    initial commit

---
+++ test expand bug/DetailViewController.swift
@@ -0,0 +1,48 @@
+//
+//  DetailViewController.swift
+//  test expand bug
+//
+//  Created by Sergey Galezdinov on 22.11.16.
+
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+        if let item = self.splitViewController?.displayModeButtonItem {
+            self.navigationItem.leftBarButtonItem = item
+            self.navigationItem.leftItemsSupplementBackButton = true
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
